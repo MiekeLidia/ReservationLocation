9b7668ea3f03deafe7b81703782b170ebfd63078
Lidia <60886622+MiekeLidia@users.noreply.github.com>
2021-10-23 18:34:22 +0200

    update

--- src/LocationContext/Adapter/LocationRESTService.java
+++ src/LocationContext/Adapter/LocationRESTService.java
@@ -1,36 +1,31 @@
-//package LocationContext.Addapter;
-//
-//import LocationContext.Application.DeskApplicationService;
-//import LocationContext.Application.RoomApplicationService;
-//import LocationContext.domain.DeskType;
-//import LocationContext.domain.Floor;
-//import LocationContext.domain.Location;
-//import LocationContext.domain.Room;
-//import LocationContext.domain.repositories.DeskRepository;
-//import LocationContext.domain.repositories.FloorRepository;
-//import LocationContext.domain.repositories.LocationRepository;
-//import LocationContext.domain.repositories.RoomRepository;
-//
-//public class LocationRESTService {
-//    public RoomApplicationService roomApplicationService;
-//
-//
-//    public LocationRESTService(RoomApplicationService roomApplicationService){
-//        DeskRepository deskRepository = new DeskRepository();
-//        RoomRepository roomRepository = new
-//        this.roomApplicationService = new RoomApplicationService( deskRepository,  roomRepository);
-//
-//    }
-//
-//    public Room roomUnavailable(Long roomId){
-//        roomApplicationService.roomUnavailable(roomId);
-//        return new Room();
-//    }
-//
-//    public boolean addDeskToRoom(long roomId, long adminId, long locationId, long floorId, long deskId, boolean computerUsable, boolean currentlyUsable, boolean sockets, DeskType deskType){
-//        roomApplicationService.addDeskToRoom(adminId, roomId, deskId, computerUsable, currentlyUsable, sockets, floorId,  deskType, locationId);
-//
-//    }
-//
-//
-//}
+package LocationContext.Addapter;
+
+import LocationContext.Application.DeskApplicationService;
+import LocationContext.Application.RoomApplicationService;
+import LocationContext.domain.DeskType;
+import LocationContext.domain.Floor;
+import LocationContext.domain.Location;
+import LocationContext.domain.Room;
+import LocationContext.domain.repositories.DeskRepository;
+import LocationContext.domain.repositories.FloorRepository;
+import LocationContext.domain.repositories.LocationRepository;
+import LocationContext.domain.repositories.RoomRepository;
+
+public class LocationRESTService {
+    public RoomApplicationService roomApplicationService;
+
+
+    public LocationRESTService(RoomApplicationService roomApplicationService){
+        DeskRepository deskRepository = new DeskRepository();
+        RoomRepository roomRepository = new
+        this.roomApplicationService = new RoomApplicationService( deskRepository,  roomRepository);
+
+    }
+
+    public Room roomUnavailable(Long roomId){
+        roomApplicationService.roomUnavailable(roomId);
+        return new Room();
+    }
+
+
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,44 @@
 package sample;
 
-import LocationContext.Adapter.LocationRESTService;
+//import LocationContext.Adapter.LocationRESTService;
+
+import LocationContext.Adapter.RoomRESTService;
+import LocationContext.Application.RoomApplicationService;
+import LocationContext.domain.DeskType;
 
 public class Main {
 
     public static void main(String[] args) {
-        roomPutUnavailable(10L);
+//        roomPutUnavailable(10L);
+        Long roomId = 1L;
+        Long adminId = 1L;
+        Long locationId = 1L;
+        Long floorId = 1L;
+        Long deskId = 100L;
+        boolean computerUsable = true;
+        boolean currentlyUsable = true;
+        boolean sockets = false;
+        DeskType deskType = DeskType.STANDAARD_DESK;
+
+
+
+
+        addDeskToRoom(roomId, adminId, locationId, floorId, deskId, computerUsable, currentlyUsable, sockets, deskType);
+
     }
 
-    public static void roomPutUnavailable(Long roomId){
-        LocationRESTService locationRESTService = new LocationRESTService();
-        locationRESTService.roomUnavailable(roomId);
-    }
+//    public static void roomPutUnavailable(Long roomId){
+//        LocationRESTService locationRESTService = new LocationRESTService();
+//        locationRESTService.roomUnavailable(roomId);
+//    }
 
+    public static void addDeskToRoom(Long roomId, Long adminId, Long locationId, Long floorId, Long deskId, boolean computerUsable, boolean currentlyUsable, boolean sockets, DeskType deskType){
+
+        RoomRESTService roomRESTService = new RoomRESTService(new RoomApplicationService());
+        roomRESTService.addDeskToRoom(roomId, adminId, locationId, floorId, deskId, computerUsable, currentlyUsable, sockets, deskType);
+
+
+    }
 }
 
 /* import javafx.application.Application;
