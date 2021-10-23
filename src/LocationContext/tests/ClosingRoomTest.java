e21ea8182eef3e8beeab8c444d9029c1a45e510f
Ishara-MacDonald <ishara.macdonald@student.hu.nl>
2021-10-23 19:24:10 +0200

    Created Setup

--- src/LocationContext/tests/ClosingRoomTest.java
+++ src/LocationContext/tests/ClosingRoomTest.java
@@ -1,15 +1,31 @@
 package LocationContext.tests;
 
+import LocationContext.Application.DeskApplicationService;
+import LocationContext.Application.RoomApplicationService;
+import LocationContext.domain.repositories.DeskRepository;
+import LocationContext.domain.repositories.RoomRepository;
+import LocationContext.infrastructure.persistence.HibernateDeskRepository;
+import LocationContext.infrastructure.persistence.HibernateRoomRepository;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 
 class ClosingRoomTest {
+    private RoomRepository roomRepository;
+    private DeskRepository deskRepository;
+    private RoomApplicationService roomApplicationService;
+    private DeskApplicationService deskApplicationService;
 
     @BeforeEach
     void setUp(){
         System.out.println("Setting up system...");
+        this.roomRepository = new HibernateRoomRepository();
+        this.deskRepository = new HibernateDeskRepository();
+        this.roomApplicationService = new RoomApplicationService(roomRepository, deskRepository);
+        this.deskApplicationService = new DeskApplicationService(deskRepository, roomRepository);
+        roomApplicationService.setDeskApplicationService(deskApplicationService);
+        deskApplicationService.setRoomApplicationService(roomApplicationService);
     }
 
     @Test
